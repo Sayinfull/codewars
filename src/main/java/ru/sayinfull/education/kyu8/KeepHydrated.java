745b2fc4c7a4baff222d8800acc58add70851d2a
Sayinfull <magen-96@mail.ru>
2022-11-16 19:32:29 +0500

    Дальше больше

--- src/main/java/ru/sayinfull/education/kyu8/KeepHydrated.java
+++ src/main/java/ru/sayinfull/education/kyu8/KeepHydrated.java
@@ -2,11 +2,11 @@ package ru.sayinfull.education.kyu8;
 
 public class KeepHydrated {
     public static void main(String[] args) {
-        System.out.println(Liters(2));
+        System.out.println(Liters(9.9));
     }
     public static int Liters(double time)  {
 
-        return (int) Math.ceil(time/0.5);
+        return (int) Math.floor(time*0.5);
 
     }
 }
