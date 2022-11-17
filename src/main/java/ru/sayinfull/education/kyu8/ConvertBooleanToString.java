f6f27aaba1d74ab3a197ee6e6964790cb9bd318b
Sayinfull <magen-96@mail.ru>
2022-11-17 16:54:04 +0500

    Implement a function which convert the given boolean value into its string representation.
    Note: Only valid inputs will be given.

---
+++ src/main/java/ru/sayinfull/education/kyu8/ConvertBooleanToString.java
@@ -0,0 +1,15 @@
+package ru.sayinfull.education.kyu8;
+
+
+public class ConvertBooleanToString {
+    public static void main(String[] args) {
+        System.out.println(convert(false));
+    }
+    public static String convert(boolean b) {
+        if (true) {
+            return String.valueOf(b);
+        } else {
+            return String.valueOf(b);
+        }
+    }
+}
